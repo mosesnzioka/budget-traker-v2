2fb86191ff725900f81194119a7450faa0deb3f1
mosesnzioka <mosesnzioka1@gmail.com>
2024-10-29 21:58:44 +0300

    adding update,getting many items and deleting item in the app

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ app.get("/Items", async (req, res) => {
   try{
     const allitems = await client.budget.findMany()
     if(allitems <= 0){
-      res.status(204).json({message: "you don't have Items yet"})
+      res.status(200).json({message: "you don't have Items yet"})
     } else{
       res.status(200).json({data : allitems})
     }
@@ -75,14 +75,51 @@ app.get("/Items/:title", async (req, res) => {
 
 //Updating an Item /Items/:title
 
-app.patch("/Items/:title", (req, res) => {
-  res.send("Updating an Item");
+app.patch("/Items/:title", async (req, res) => {
+  const wantedItemtitle = req.params.title;
+  const { title, quantity, unitprice } = req.body;
+  try{
+    let updatedItem;
+    if(title){
+      updatedItem = await client.budget.update({
+        where: {title: wantedItemtitle},
+        data : { title: title}
+      })
+    }
+    if(quantity){
+      updatedItem = await client.budget.update({
+        where: {title: wantedItemtitle},
+        data : { quantity: quantity}
+      })
+    }
+    if(unitprice){
+      updatedItem = await client.budget.update({
+        where: {title: wantedItemtitle},
+        data : { unitprice: unitprice}
+      })
+
+    }
+    res.status(200).json({message: "Item updated succesifuly", data : updatedItem})
+
+  }catch (e){
+    res.status(500).json({message: "server error"})
+  }
 });
 
 //deleting an Item /Items/:title
 
-app.delete("/Items/:title", (req, res) => {
-  res.send("Deleting an Item");
+app.delete("/Items/:title", async (req, res) => {
+  const title =req.params.title
+  try{
+    await client.budget.delete({
+      where: {title: title}
+    })
+    res.status(200).json({message: "Item deleted succsefuly"})
+
+  }catch (e){
+  
+    res.status(500).json({message: "server error"})
+  }
 });
 
 app.listen(3000, () => {
